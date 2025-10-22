1dbe28352e44671399d75d48c977b8c72cf6ec13
Md-Siyam94 <siyam942404.com>
2025-10-22 20:15:33 +0600

    make a user collection and upload api

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5600
+
+app.use(express.json()) 
+app.use(cors())
+
+
+app.get('/', (req, res)=>{
+    res.send('This is Twine server')
+})
+
+// Twine
+// BeI7Zq916a1LrZXz
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.ttcu5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(process.env.DB_USER);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+
+    const productCollection = client.db("Twine").collection('products')
+    const userCollection = client.db("Twine").collection("users")
+
+
+    // User Collection
+
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.get('/users/:email', async(req, res)=>{
+      const email = req.params.email;
+      const filter = {email: email};
+      const result = await userCollection.findOne(filter);
+      // console.log(result);
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      // console.log(query);
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({ message: "User already exist", insertedId: null })
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+
+
+
+    // Products Collection
+
+    app.get("/products", async(req, res)=>{
+      const result= await productCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post("/products", async(req, res)=>{
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result)
+    })
+
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`The server is running of port ${port}`);
+})
