1438655edada0042a5bf9595241cf6d66a050932
Md-Siyam94 <siyam942404.com>
2025-10-30 18:06:32 +0600

    make the user collection and get and post api is ready

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Product = require('../models/Product');
+const Products = require('../models/Product');
 
 
 const router = express.Router()
@@ -7,15 +8,22 @@ const router = express.Router()
 
 // get products
     router.get('/', async (req, res)=>{
-        const result = await Product.find();
-        res.json(result)
+        const products = await Product.find();
+        res.json(products)
     })
 
     // post product
-    router.post('/create', async(req,res)=>{
+    router.post('/', async(req,res)=>{
         const product = new Product(req.body)
         await product.save()
         res.status(201).json({success: true, data: product})
+    })
+
+    // get single product
+    router.get("/:id", async(req, res)=>{
+        const id = req.params.id;
+        const product = await Products.findById(id);
+        res.json(product)
     })
 
 
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/User');
 const { model } = require('mongoose');
 const {default: mongoose} = require('mongoose');
+const Products = require('../models/Product');
 
 const router = express.Router();
 
@@ -10,8 +11,23 @@ router.get("/", async(req, res)=>{
     res.json(result)
 })
 
+router.get("/:email", async(req, res)=>{
+    const email = req.params.email;
+    const filter = {email: email};
+    const result = await User.findOne(filter);
+    res.json(result)
+    // console.log(result);
+})
+
 router.post("/", async(req, res)=>{
-    const user = req.body;
+    const user = new User(req.body);
+    const email = {email: user?.email}
+    const existingUser = await User.findOne(email)
+    if(existingUser){
+       return res.json({ message: "User already exist", insertedId: null })
+    }
+    await user.save();
+    res.status(201).json({success: true, data: user})
     // const result = await User
     // console.log(user);
 })
