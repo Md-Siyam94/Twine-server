2a3c58a4bd2dbbcc4e888308ca0e91a52842ba9b
Md-Siyam94 <siyam942404.com>
2025-11-12 21:38:20 +0600

    get cart products what individual added

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -3,41 +3,51 @@ const Cart = require('../models/Cart');
 
 const router = express.Router()
 
+// post at cart
+router.post("/", async (req, res) => {
+    const product = new Cart(req.body);
+    const productId = { productId: product?.productId };
+    // console.log(productId);
+    const existingProduct = await Cart.findOne(productId);
+    // console.log(existingProduct);
+    if (existingProduct) {
+        return res.json({ message: "Product already have in cart" })
+    }
+    await product.save()
+    res.status(201).json({ success: true, data: product })
+})
+
+
 // get cart products
-    router.get("/", async(req, res)=>{
-        const cartProducts = await Cart.find()
-        res.json(cartProducts)
-    })
+router.get("/", async (req, res) => {
+    const cartProducts = await Cart.find()
+    res.json(cartProducts)
+})
 
-    // post at cart
-    router.post("/", async(req, res)=>{
-        const product = new Cart(req.body); 
-        const productId = {productId: product?.productId};
-        // console.log(productId);
-        const existingProduct = await Cart.findOne(productId);
-        console.log(existingProduct);
-        if(existingProduct){
-           return res.json({message: "Product already have in cart"})
-        }
-        await product.save()
-        res.status(201).json({success: true, data: product})
-    })
+// get product by email
+router.get("/:email", async(req, res)=>{
+    const email = req.params.email;
+    const filter = {userEmail: email};
+    const products = await Cart.find(filter);
+    res.json(products)
+})
 
-    // get single product from cart
-    router.get("/:id", async(req, res)=>{
-        const id = req.params.id;
-        const product = await Cart.findById(id);
-        res.json(product)
 
-    })
+// get single product from cart
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const product = await Cart.findById(id);
+    res.json(product)
 
-    // delete product from cart
-    router.delete("/:id", async(req, res)=>{
-        const id = req.params.id;
-        const filter = {_id: id}
-        const result = await Cart.deleteOne(filter)
-        res.json(result)
-    })
+})
+
+// delete product from cart
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: id }
+    const result = await Cart.deleteOne(filter)
+    res.json(result)
+})
 
 
 
