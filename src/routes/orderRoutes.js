cb459a9a040c73f2e4d88a97f624683e49107fcc
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-11-22 19:58:27 +0600

    get Men's product

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,7 +4,7 @@ const Order = require('../models/Order');
 const router = express.Router()
 
 // get all orders
-router.get("/")
+// router.get("/")
 
 
 module.exports = router
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -6,37 +6,53 @@ const Products = require('../models/Product');
 const router = express.Router()
 
 
+
+// post product
+router.post('/', async (req, res) => {
+    const product = new Product(req.body)
+    await product.save()
+    res.status(201).json({ success: true, data: product })
+})
+
 // get products
-    router.get('/', async (req, res)=>{
-        const products = await Product.find();
-        res.json(products)
-    })
+router.get('/', async (req, res) => {
+    const products = await Product.find();
+    res.json(products)
+})
 
-    // post product
-    router.post('/', async(req,res)=>{
-        const product = new Product(req.body)
-        await product.save()
-        res.status(201).json({success: true, data: product})
-    })
+// get men's products
+router.get('/men-products', async (req, res) => {
+    const query = { targetAudience: "Men" }
+    const products = await Product.find(query)
+    // console.log(products);
+    res.json(products)
+})
+// get women's products
+router.get('/women-products', async (req, res) => {
+    const query = { targetAudience: "Women" }
+    const products = await Product.find(query)
+    // console.log(products);
+    res.json(products)
+})
 
-    // get single product
-    router.get("/:id", async(req, res)=>{
-        const id = req.params.id;
-        console.log(id);
-        const filter = {_id: id}
-        console.log(filter);
-        const product = await Products.findOne(filter);
-        res.json(product)
-    })
+// get single product
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const filter = { _id: id }
+    // console.log(filter);
+    const product = await Products.findOne(filter);
+    res.json(product)
+})
 
-    // delete single product
-    router.delete("/:id", async(req, res)=>{
-        const id = req.params.id;
-        const filter = {_id: id};
-        const result = await Product.deleteOne(filter);
-        res.json(result)
-    })
+// delete single product
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: id };
+    const result = await Product.deleteOne(filter);
+    res.json(result)
+})
 
 
 
-    module.exports = router
+module.exports = router
