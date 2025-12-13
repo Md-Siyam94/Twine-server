390577a657e725acc39b99062560cb62159dd6df
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-12-13 22:01:32 +0600

    complete verify token middlewere

--- src/middleweres/verifyToken.js
+++ src/middleweres/verifyToken.js
@@ -1,12 +1,21 @@
+const jwt = require('jsonwebtoken');
+
 // verify token
 const verifyToken = (req, res, next) => {
     // console.log(req.headers.authorization);
     if (!req.headers.authorization) {
         return res.status(401).json({ message: 'Forbidden access' })
     }
-    const token = req.headers?.authorization.splite(' ')[1]
+    const token = req.headers?.authorization.split(' ')[1]
     console.log(token);
-    next()
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+    
 }
 
 module.exports = verifyToken
