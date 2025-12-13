88da3b8054c76e061e76eb1018c099155aba19cb
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-12-13 23:05:32 +0600

    make is admin api

--- src/middleweres/verifyToken.js
+++ src/middleweres/verifyToken.js
@@ -4,13 +4,13 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     // console.log(req.headers.authorization);
     if (!req.headers.authorization) {
-        return res.status(401).json({ message: 'Forbidden access' })
+        return res.status(401).json({ message: 'Unauthorized access' })
     }
     const token = req.headers?.authorization.split(' ')[1]
-    console.log(token);
+    // console.log(token);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ message: 'Forbidden access' })
+            return res.status(401).json({ message: 'Unauthorized access' })
         }
         req.decoded = decoded
         next()
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,10 +3,11 @@ const User = require('../models/User');
 const { model } = require('mongoose');
 const {default: mongoose} = require('mongoose');
 const Products = require('../models/Product');
+const verifyToken = require('../middleweres/verifyToken');
 
 const router = express.Router();
 
-router.get("/", async(req, res)=>{
+router.get("/",verifyToken, async(req, res)=>{
     const result = await User.find();
     return res.json(result)
 })
@@ -30,6 +31,21 @@ router.post("/", async(req, res)=>{
     return res.status(201).json({success: true, data: user})
     // const result = await User
     // console.log(user);
+})
+
+router.get('/admin/:email', async(req, res)=>{
+    const email = req.params.email;
+    if(!email === req.decoded.email ){
+        return res.status(401).json({message: "Unauthorized access!"})
+    }
+    const filter = {email: email};
+    const user = await User.findOne(filter);
+    let isAdmin = false;
+    if(user?.role === 'admin'){
+        isAdmin = true
+    };
+    return res.json(isAdmin)
+
 })
 
 module.exports = router
