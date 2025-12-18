92155d3bd331a330ad7e0dabf0c479f48ef2215a
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-12-18 20:24:26 +0600

    create payment schema

---
+++ src/models/Payment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = mongoose.Schema({
+    
+
+})
+
+const Payment = mongoose.model("Payments", paymentSchema)
+module.exports = Payment
--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -12,4 +12,8 @@
 // Validation API: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php?wsdl
 // Validation API (Web Service) name: https://sandbox.sslcommerz.com/validator/api/validationserverAPI.php
 
+const express = require('express');
+const router = express.Router()
 // initiate payment
+
+    router.post('/create-ssl-payment')
